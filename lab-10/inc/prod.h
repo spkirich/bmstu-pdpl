3e0aa9ee710b51291bdb32801b875f3be25d7708
Sergey Kirichenko <s2002kir@yandex.ru>
2022-05-10 16:30:05 +0300

    Add prod module interface

---
+++ lab-10/inc/prod.h
@@ -0,0 +1,10 @@
+#ifndef PROD_H
+#define PROD_H
+
+//! Простое скалярное произведение
+float sdprod(float *, float *);
+
+//! Быстрое скалярное произведение
+float fdprod(float *, float *);
+
+#endif // PROD_H
