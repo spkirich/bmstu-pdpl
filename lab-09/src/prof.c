803b70c4a66dfde0752be111fe71b03424e65010
Sergey Kirichenko <s2002kir@yandex.ru>
2022-05-13 21:56:18 +0300

    Improve macro

--- lab-09/src/prof.c
+++ lab-09/src/prof.c
@@ -11,7 +11,7 @@
 #define LOOP 1e+8
 
 // Не очень приятный макрос для профилирования разных функций
-#define PROF(desc, func) do {                               \
+#define PROF(func) do {                                     \
     clock_t s = clock();                                    \
                                                             \
     for (int i = 0; i < LOOP; i++)                          \
@@ -20,29 +20,29 @@
     clock_t t = clock();                                    \
                                                             \
     printf("%s: %e s\n",                                    \
-        desc, (double) (t - s) / CLOCKS_PER_SEC / LOOP);    \
+        #func, (double) (t - s) / CLOCKS_PER_SEC / LOOP);   \
 } while (0)
 
 int main() {
-    PROF("Soft 32 + 32", sadd32);
-    PROF("Soft 32 * 32", smul32);
+    PROF(sadd32);
+    PROF(smul32);
 
-    PROF("Soft 64 + 64", sadd64);
-    PROF("Soft 64 * 64", smul64);
+    PROF(sadd64);
+    PROF(smul64);
 
 #ifdef X87
 
-    PROF("Soft 80 + 80", sadd80);
-    PROF("Soft 80 * 80", smul80);
+    PROF(sadd80);
+    PROF(smul80);
 
-    PROF("Hard 32 + 32", hadd32);
-    PROF("Hard 32 * 32", hmul32);
+    PROF(hadd32);
+    PROF(hmul32);
 
-    PROF("Hard 64 + 64", hadd64);
-    PROF("Hard 64 * 64", hmul64);
+    PROF(hadd64);
+    PROF(hmul64);
 
-    PROF("Hard 80 + 80", hadd80);
-    PROF("Hard 80 * 80", hmul80);
+    PROF(hadd80);
+    PROF(hmul80);
 
 #endif // X87
 
